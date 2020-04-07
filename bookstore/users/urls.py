4ebadad1fddcf98dea25e93d49070900f90d6d9f
doigtt2 <damiantorrens@yahoo.com>
2020-04-07 18:11:38 -0400

    Password reset done

--- bookstore/users/urls.py
+++ bookstore/users/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('profile/', ProfileView, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
 ]
